be13c1391adb9ac6fee8d4a1093ed6dbcfb51b65
Paul Mc Auley <paul@roscarraig.com>
2021-12-05 20:36:46 +0000

    Day 4 Rust first cut

---
+++ day4/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::vec::Vec;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if let Ok(lines) = read_lines(&args[1])
+    {
+        for line in lines
+        {
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
