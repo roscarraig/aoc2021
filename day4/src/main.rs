e47cee0764096cba7525ba75562cbcbae9099c28
Paul Mc Auley <paul@roscarraig.com>
2021-12-07 01:04:56 +0000

    More interim

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,40 +1,25 @@
 use std::env;
 use std::vec::Vec;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs;
 
 struct Card {
-    marked: Vec<u8>,
-    grid: [[u8; 5]; 5],
+    marked: Vec<usize>,
+    grid: [[usize; 5]; 5],
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let filename = &args[1].to_string();
+    let data = fs::read_to_string(filename).expect("Unable to open file");
+    let mut blocks = data.split("\n\n");
+    // let mut blocks = fs::read_to_string(filename).split("\n\n");
+    let calls = blocks.next().unwrap().split(',').map(|n| n.parse().unwrap());
+    let mut cards: Vec<Card> = blocks.map(
+        |card| card.lines().map(
+            |line| line.split_whitespace().map(
+                |n| n.parse().ok()
+            ).collect()
+        ).collect()
+    ).collect();
 
-    if let Ok(mut lines) = read_lines(&args[1])
-    {
-        println!("Hello!");
-        let pline = lines.next();
-        println!("{:?}", pline.unwrap());
-        // let parts = lines.next().unwrap().split(",");
-        // let calls = parts.collect::<Vec<&str>>();
-        // for item in calls
-        // {
-        //     println!("{}", item);
-        // }
-        // println!("=====");
-        // for line in lines
-        // {
-            // println!("{}", line.unwrap());
-        // }
-    }
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
